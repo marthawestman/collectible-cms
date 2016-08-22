cc3e1c7acf52c01cbacb1ba5f47b2250290072c7
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2016-08-22 15:34:38 -0700

    Made main page aware of site configuration.

--- client/app/routes/site/home/component.ts
+++ client/app/routes/site/home/component.ts
@@ -2,7 +2,9 @@ import { Component }	                 from '@angular/core';
 import { OnInit }		                 from '@angular/core';
 import { AlertMessage }                  from '../../../models/alertMessage';
 import { User, CurrentUser }			 from '../../../models/user';
+import { Config, ConfigContainer }       from '../../../models/config';
 import { AuthenticateService }           from '../../../services/authenticate/authenticate.service';
+import { ConfigService }                 from '../../../services/config/config.service';
 
 @Component({
     moduleId: module.id,	
@@ -14,9 +16,20 @@ import { AuthenticateService }           from '../../../services/authenticate/au
 export class RoutesSiteHomeComponent implements OnInit {
 	alerts: AlertMessage[] = [];
 	currentUser: CurrentUser = null;
-    constructor(private authService: AuthenticateService) {
+    configContainer: ConfigContainer = new ConfigContainer();
+    loaded = false;
+    working = false;
+    constructor(private authService: AuthenticateService, private configService: ConfigService) {
     }
     ngOnInit() {
     	this.currentUser = this.authService.getCurrentUser();
+        this.configService.read().subscribe(
+            configContainer => {
+                this.configContainer = configContainer;
+                this.loaded = true;
+            },
+            err => { this.alerts.push({ type: 'error', message: err }); },
+            () => { this.working = false; }
+        );
     }
 };
